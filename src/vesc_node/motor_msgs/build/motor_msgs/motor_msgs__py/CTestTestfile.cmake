628785d622c947a749a74c3020d2fcc340bfa003
Seth M King <nerd.king@gatech.edu>
2019-06-19 07:33:44 -0400

    cleaning up dirs that should have been ignored.

--- src/vesc_node/motor_msgs/build/motor_msgs/motor_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/repos/ROS_2_VESC_Node/src/vesc_node/motor_msgs/build/motor_msgs/motor_msgs__py
-# Build directory: /opt/repos/ROS_2_VESC_Node/src/vesc_node/motor_msgs/build/motor_msgs/motor_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/vesc_node/motor_msgs/install/motor_msgs/lib/python3.6/site-packages/motor_msgs/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from motor_msgs.msg._motor_data import MotorData  # noqa: F401
